e3a387737b6eeddf72e2fdc504e6b69aed783062
IndraRusyana <indrarusyana@gmail.com>
2023-12-16 19:09:06 +0700

    konfig database,web server dan rest api

---
+++ app.js
@@ -0,0 +1,52 @@
+require('dotenv').config();
+var cors = require('cors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+// var authRouter = require('./routes/auth');
+var indexRouter = require('./routes/index');
+// var satwaRouter = require('./routes/satwa');
+// var donasiRouter = require('./routes/donasi');
+var userRouter = require('./routes/users');
+// var satwaDonasiRouter = require('./routes/satwaDonasi');
+// var satwaGambarRouter = require('./routes/satwaGambar');
+// var predictRouter = require('./routes/predict');
+// var transaksiRouter = require('./routes/transaksi');
+
+var app = express();
+
+let whitelist = ['http://localhost:3000', 'http://localhost', 'http://35.219.90.56:3000'];
+
+app.use(cors({
+  credentials: true,
+  origin: function (origin, callback) {
+    // allow requests with no origin 
+    if (!origin) return callback(null, true);
+    if (whitelist.indexOf(origin) === -1) {
+      var message = 'The CORS policy for this origin doesn\'t ' +
+        'allow access from the particular origin.';
+      return callback(new Error(message), false);
+    }
+    return callback(null, true);
+  },
+  exposedHeaders: ['set-cookie']
+}));
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// app.use('/auth', authRouter);
+app.use('/', indexRouter);
+// app.use('/satwa', satwaRouter);
+// app.use('/donasi', donasiRouter);
+app.use('/user', userRouter);
+// app.use('/satwa-donasi', satwaDonasiRouter);
+// app.use('/satwa-gambar', satwaGambarRouter);
+// app.use('/predict', predictRouter);
+// app.use('/transaksi', transaksiRouter);
+
+module.exports = app;
---
+++ migrations/20231216021117-create-user.js
@@ -0,0 +1,57 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      email: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      name: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      date_of_birth: {
+        allowNull: true,
+        type: Sequelize.DATE
+      },
+      gender: {
+        allowNull: true,
+        type: Sequelize.STRING
+      },
+      weight: {
+        allowNull: true,
+        type: Sequelize.INTEGER
+      },
+      height: {
+        allowNull: true,
+        type: Sequelize.INTEGER
+      },
+      purpose: {
+        allowNull: true,
+        type: Sequelize.STRING
+      },
+      password: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Users');
+  }
+};
